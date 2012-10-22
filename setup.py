84abc33a7a4bde22f60bed89fa2dbb5337426580
sam <sam.sandberg@zemanta.com>
2012-10-22 16:16:52 -0400

    Moving the libs around a bit and writing a setup.py file for easier installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(name='raventools',
+	version='0.1',
+	description='Raven Tools API Wrapper',
+	author='Sam Sandberg',
+	author_email='sam.sandberg@gmail.com',
+	url='https://api.raventools.com/docs/',
+	packages=['raventools'] + [ 'raventools.%s' % package for package in find_packages('.') ],
+)
