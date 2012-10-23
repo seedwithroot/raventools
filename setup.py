aae129382785bb3201a9a7bef53b01bf6dff6ba6
sam <sam.sandberg@zemanta.com>
2012-10-23 17:08:24 -0400

    bumping the version to 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='raventools',
-	version='0.2',
+	version='0.3',
 	description='Raven Tools API Wrapper',
 	author='Sam Sandberg',
 	author_email='sam.sandberg@gmail.com',
