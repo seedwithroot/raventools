ad6a4a78b7e927d9f29bab82a6afaa4a402aa7b3
sam <sam.sandberg@zemanta.com>
2012-10-23 16:52:13 -0400

    Apparently the last commit had 0.1 in setup.py so I'm bumping to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='raventools',
-	version='0.1',
+	version='0.2',
 	description='Raven Tools API Wrapper',
 	author='Sam Sandberg',
 	author_email='sam.sandberg@gmail.com',
